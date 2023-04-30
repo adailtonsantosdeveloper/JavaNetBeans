5f5cb0932281e4caffde289582724b6e5c6bb83e
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-04-30 12:36:13 -0300

    Arquivo introdutório sobre a linguagem Java

---
+++ JavaProjectPublic/src/introducaojava/IntroducaoJava.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package introducaojava;
+
+/**
+ *
+ * @author ADAILTON
+ */
+public class IntroducaoJava {
+    
+    public static void main(String[] args) {
+        System.out.println("************ Bem vindos à introdução da linguagem Java! **************");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        System.out.println("Java é uma linguagem de programação multiplataforma.");
+        System.out.println("Mas, O que isso significa?");
+        System.out.println("Ser multiplataforma significa que um programa desenvolvido em Java");
+        System.out.println("pode funcionar em diversos sistemas, tais como: ");
+        System.out.println("Windows, MacOS, Linux, Android, microcontroladores, Sistemas Embarcados, etc.");
+        System.out.println("Este feito ocorre graças à JVM - Java Virtual Machine (Máquina Virtual Java).");
+        System.out.println("Máquina Virtual é um software que imita uma máquina real, é um ecossistema próprio"+
+                " do Java que precisa ser instalado previamente no sistema real para que o Java funcione.");
+        
+    }
+    
+}
